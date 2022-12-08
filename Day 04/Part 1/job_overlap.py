c5b1e46a91ee2d4c75941c691af6d6f3765f4807
root <root@ZACH-ALIENWARE.localdomain>
2022-12-07 18:36:38 -0700

    As with many things, I was over complicating the solution

--- Day 04/Part 1/job_overlap.py
+++ Day 04/Part 1/job_overlap.py
@@ -1,25 +1,19 @@
 from os.path import realpath
-import pdb
 
-input = []
 output = 0
 
-#Read the input file to a local list of the lines.
-with open(realpath('input.txt'),'r') as f:
+#Read the input file and see if the jobs overlap
+with open(realpath('.\\Day 04\\input.txt'),'r') as f:
 	for line in f:
-		pdb.set_trace()
 		jobs = line.split(',')
 		job_0 = jobs[0].split('-')
 		job_1 = jobs[1].split('-')
 
-		if int(job_0[1]) - int(job_0[0]) > int(job_1[1]) - int(job_1[0]):
-			#job_0 covers more space than job_1
-			if job_0[0] <= job_1[0] and job_0[1] >= job_1[1]:
-				#job_0 starts before and ends after job_1
-				output += 1
-		else:
-			#job_1 covers more space than job_0, or they have the same space
-			if job_1[0] <= job_0[0] and job_1[1] >= job_0[1]:
-				#job_1 starts before and ends after job_0
-				output += 1
+		if int(job_0[0])<= int(job_1[0]) and int(job_0[1]) >= int(job_1[1]):
+			# Job 0 starts before/at AND ends after/at job 2
+			output += 1
+		elif int(job_1[0])<= int(job_0[0]) and int(job_1[1]) >= int(job_0[1]):
+			#Job 1 starts at/before Job 0 AND ends at/after Job 0
+			output += 1
+		
 print(output)
